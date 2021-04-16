55fc01187eae6f0d87f75c42695bbe9f4d821988
NinMario64 <xxninmario64xx@gmail.com>
2021-04-15 23:05:35 -0400

    Now with content :)

---
+++ src/attributes.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    let attributes = {
+        'HP':'Health',
+        'MP':'Mana Points',
+        'STR':'Strength',
+        'INT':'Intelligence',
+        'AGI':'Agility',
+        'END':'Endurance',
+        'DEF':'Defense',
+        'RES':'Resistance',
+        'CHA':'Charisma',
+        'LCK':'Luck'
+    }
+    </script>
